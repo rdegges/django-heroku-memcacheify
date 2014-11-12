308b9270023e983184a3dd0737098fe622987e1e
Randall Degges <r@rdegges.com>
2014-11-12 13:16:43 -0800

    Fixing spacing.

--- memcacheify.py
+++ memcacheify.py
@@ -30,13 +30,14 @@ MEMCACHEDCLOUD_ENV_VARS = (
 # Memcache defaults
 # Both Memcachier and MemcacheCloud share these
 CACHE_DEFAULTS = {
-            'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-            'BINARY': True,
-            'OPTIONS': {
-                'ketama': True,
-                'tcp_nodelay': True,
-            },
-        }
+    'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+    'BINARY': True,
+    'OPTIONS': {
+        'ketama': True,
+        'tcp_nodelay': True,
+    },
+}
+
 
 def memcacheify(timeout=500):
     """Return a fully configured Django ``CACHES`` setting. We do this by
@@ -52,27 +53,30 @@ def memcacheify(timeout=500):
 
     if all((environ.get(e, '') for e in MEMCACHE_ENV_VARS)):
         caches['default'] = CACHE_DEFAULTS
-        caches['default'].update({'LOCATION': 'localhost:11211',
-                                  'TIMEOUT': timeout,
-                                 })
+        caches['default'].update({
+            'LOCATION': 'localhost:11211',
+            'TIMEOUT': timeout,
+        })
     elif all((environ.get(e, '') for e in MEMCACHIER_ENV_VARS)):
         servers = environ.get('MEMCACHIER_SERVERS').replace(',', ';')
         environ['MEMCACHE_SERVERS'] = servers
         environ['MEMCACHE_USERNAME'] = environ.get('MEMCACHIER_USERNAME')
         environ['MEMCACHE_PASSWORD'] = environ.get('MEMCACHIER_PASSWORD')
         caches['default'] = CACHE_DEFAULTS
-        caches['default'].update({'LOCATION': servers,
-                                  'TIMEOUT': timeout,
-                                 })
+        caches['default'].update({
+            'LOCATION': servers,
+            'TIMEOUT': timeout,
+        })
     elif all((environ.get(e, '') for e in MEMCACHEDCLOUD_ENV_VARS)):
         servers = environ.get('MEMCACHEDCLOUD_SERVERS').replace(',', ';')
         environ['MEMCACHE_SERVERS'] = servers
         environ['MEMCACHE_USERNAME'] = environ.get('MEMCACHEDCLOUD_USERNAME')
         environ['MEMCACHE_PASSWORD'] = environ.get('MEMCACHEDCLOUD_PASSWORD')
         caches['default'] = CACHE_DEFAULTS
-        caches['default'].update({'LOCATION': servers,
-                                  'TIMEOUT': timeout,
-                                 })
+        caches['default'].update({
+            'LOCATION': servers,
+            'TIMEOUT': timeout,
+        })
     elif environ.get('MEMCACHEIFY_USE_LOCAL', False):
         caches['default'] = {
             'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
